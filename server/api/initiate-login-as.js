baca2e76fccdf1c7607ad2deb06a4ea0b71e9ca8
AleksaButterfly <aleksaprebiracevicorona@outlook.com>
2023-05-22 12:49:30 +0200

    Fix heroku issue

--- server/api/initiate-login-as.js
+++ server/api/initiate-login-as.js
@@ -8,7 +8,7 @@ const USING_SSL = process.env.REACT_APP_SHARETRIBE_USING_SSL === 'true';
 
 // redirect_uri param used when initiating a login as authentication flow and
 // when requesting a token using an authorization code
-const loginAsRedirectUri = `${ROOT_URL.replace(/\/$/, '')}/api/login-as`;
+const loginAsRedirectUri = `${ROOT_URL?.replace(/\/$/, '')}/api/login-as`;
 
 // Cookies used for authorization code authentication.
 const stateKey = `st-${CLIENT_ID}-oauth2State`;
